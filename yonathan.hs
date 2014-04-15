359b44b974bbb4f438b486c631190931e89515eb
Jesús Espino <jesus.espino@kaleidos.net>
2014-04-15 22:27:22 +0200

    Work in progress static files and layout change

--- yonathan.hs
+++ yonathan.hs
@@ -9,12 +9,21 @@ import Data.Text
 import qualified System.FilePath.Posix as Posix
 import System.Posix.Files
 import System.Directory
+import Text.Hamlet
+import Yesod.Static
+
+staticFiles "static"
 
 data Yonathan = Yonathan
+    { getStatic :: Static }
 
 mkYesod "Yonathan" $(parseRoutesFile "config/routes")
 
-instance Yesod Yonathan
+instance Yesod Yonathan where
+    defaultLayout contents = do
+        PageContent title headTags bodyTags <- widgetToPageContent contents
+        mmsg <- getMessage
+        giveUrlRenderer $(hamletFile "templates/layout.html")
 
 pathJoin :: [Text] -> FilePath
 pathJoin texts = Posix.joinPath $ Prelude.map unpack texts
@@ -36,5 +45,6 @@ getPathR path = do
             $(whamletFile "templates/file.html")
 
 main :: IO()
-main =
+main = do
+    staticX <- static "static"
     warp 3000 Yonathan
